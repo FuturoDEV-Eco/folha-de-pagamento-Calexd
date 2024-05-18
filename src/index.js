13f318153d7694d182511125543820ed0d09e4ea
calexd <caledarvs@gmail.com>
2024-05-18 15:24:42 -0300

    feat: 🎸 Exercício 05
    Agora, crie um programa que peça o nome do funcionário, CPF, o mês do
    pagamento (Numérico) e o salário bruto.De posse dessas informações, faça
    o cálculo do salário líquido e exiba no terminal.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
 const calcularInss = require('./calculo_inss')
 const calcularImpostoRenda = require('./calculo_imposto_renda')
 const calcularSalarioLiquido = require('./calculo_salario_liquido')
+const readline = require('readline')
+
+const input = readline.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+
+let nome = ''
+let cpf = ''
+let salarioBruto = ''
+let salarioLiquido = 0
+let inss = 0
+let impostoRenda = 0
+
+
+function formatarCpf(cpf) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4')
+}
+    
+
+input.question("Qual é seu nome?: ", (nomeDigitado) => {
+    nome = nomeDigitado;
+    input.question("Digite seu CPF: ", (cpfDigitado) => {
+        cpf = formatarCpf(cpfDigitado)
+        input.question('Digite o valor de seu salario bruto: ', (salarioBrutoDigitado) => {
+            salarioBruto = salarioBrutoDigitado
+                inss = calcularInss(salarioBruto)
+                impostoRenda = calcularImpostoRenda(salarioBruto)
+                salarioLiquido = calcularSalarioLiquido(salarioBruto, inss, impostoRenda)
+                console.log('---- FOLHA DE PAGAMENTO ----\n',
+                            'nome: ', nome, '\n',
+                            'CPF:', cpf, '\n',
+                            'Salário Bruto: R$', salarioBruto, '\n',
+                            'INSS: R$', inss, '\n',
+                            'Imposto de Renda: R$', impostoRenda, '\n',
+                            'Salário Líquido: R$', salarioLiquido)
+                input.close()
+            })
+        })
+    })
+    
 
 
 
-console.log('calculo INSS: ', calcularInss(2300))
 
-console.log('Calculo Imposto renda: ', calcularImpostoRenda(2300))
 
-console.log('Salario liquido: ', calcularSalarioLiquido(2300, 207, 14.10))
