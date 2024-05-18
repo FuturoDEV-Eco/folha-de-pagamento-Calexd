df2c117b598e2625df757a40e1c261d9c8266fb8
calexd <caledarvs@gmail.com>
2024-05-18 13:48:14 -0300

    feat: 🎸 Exercício 04
    Crie uma arquivo chamado calculo_salario_liquido.js e implemente uma
    função que retorne valor do salario líquido, considerando:‌Salário
    bruto;INSS;Imposto de renda;Outros descontos.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 const calcularInss = require('./calculo_inss')
 const calcularImpostoRenda = require('./calculo_imposto_renda')
+const calcularSalarioLiquido = require('./calculo_salario_liquido')
 
-console.log('calculo INSS: ', calcularInss(2826))
 
-console.log('Calculo Imposto renda: ', calcularImpostoRenda(8000))
+
+console.log('calculo INSS: ', calcularInss(2300))
+
+console.log('Calculo Imposto renda: ', calcularImpostoRenda(2300))
+
+console.log('Salario liquido: ', calcularSalarioLiquido(2300, 207, 14.10))
